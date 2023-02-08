21001ac5e660630879435d4ab9cb3fe2e30eb30b
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-02-07 22:21:39 -0600

    Solve mild and medium challenges

---
+++ code_challenges/mild.rb
@@ -0,0 +1,22 @@
+## Mild
+
+# Write a Ruby program that defines a variable that stores an Integer.
+ #The program should print out the String "even" if the Integer is even,
+  #and the String "odd" if the Integer is odd.
+
+cars = 12
+
+
+# if cars.even?
+#     puts "even"
+# else
+#     puts "odd"
+# end
+
+
+if cars%2 == 0
+    puts "even"
+else
+    puts "odd"
+end
+
